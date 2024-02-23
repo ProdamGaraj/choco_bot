f0b2200c7c94c9dd99e6195437f471a2d7cd3316
prodamgaraj <prodgaraj@ya.ru>
2024-02-23 17:33:16 +0300

    Initial2

---
+++ main.py
@@ -0,0 +1,20 @@
+import httpx
+from fastapi import FastAPI, Request
+
+
+TOKEN = "7099779832:AAEUyjQElb6J2ocRtXsTJi68dbHPbiEv0pE"
+CHAT_ID = "748689055"
+BASE_URL = f"https://api.telegram.org/bot{TOKEN}"
+client = httpx.AsyncClient()
+
+app = FastAPI()
+
+@app.post("/post_phone")
+async def webhook(req: Request):
+    data = await req.json()
+    print(data)
+    text = data['name']
+    text += '(\n)'
+    text += data['phone']
+    await client.post(f"{BASE_URL}/sendMessage?chat_id={CHAT_ID}&text={text}", headers={'Content-Type': 'application/json'})
+    return data
